c132274fe57cdfe237a72bb00421a7782daaf5d8
nilia2020 <jorge.niglia@gmail.com>
2021-08-06 18:05:43 -0300

    proyecto completo

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        purgecss({
+            content: ["./**/*.html", "./**/*.js"],
+            defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+        require("cssnano")({
+            preset: "default",
+        }),
+    ],
 };
